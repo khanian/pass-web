7115dcfcc6ee3a31f1cd964e01b7cb95bcae6d77
khanian <khanylee@gmail.com>
2024-01-27 00:48:53 +0900

    admin

---
+++ src/main/java/com/example/pass/service/packaze/PackageModelMapper.java
@@ -0,0 +1,17 @@
+package com.example.pass.service.packaze;
+
+import com.example.pass.repository.packaze.PackageEntity;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PackageModelMapper {
+    PackageModelMapper INSTANCE = Mappers.getMapper(PackageModelMapper.class);
+
+    List<Package> map(List<PackageEntity> packageEntities);
+
+}
